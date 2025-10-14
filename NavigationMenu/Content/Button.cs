04d04b2e629fd66b3000fa5f99fe98705a9b27b4
Filippos Dimitriou <AQ241177@aquinas.ac.uk>
2025-10-14 15:31:49 +0100

    Added button classes

---
+++ NavigationMenu/Content/Button.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavigationMenu.Content
+{
+    public class Button
+    {
+        string Name;
+        string Category;
+        bool Active;
+        Vector2 Position;
+    }
+}
